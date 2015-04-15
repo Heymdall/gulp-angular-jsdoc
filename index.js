7e7d47e04eaf10cbad51075a49168ee6f52a83a9
heymdall <kitov.nn@gmail.com>
2015-04-15 16:27:38 +0300

    Init

---
+++ index.js
@@ -0,0 +1,36 @@
+var through = require('through2'),
+    path = require('path'),
+    exec = require('child_process').exec;
+
+module.exports = function (dest) {
+    var paths = [];
+    dest = path.resolve(process.cwd(), dest);
+
+    return through.obj(function (file, enc, cb) {
+        if (file.isStream()) {
+            cb();
+            return;
+        }
+        paths.push(file.path);
+
+        cb();
+    }, function (cb) {
+        var jsdocPath = path.resolve(__dirname, './node_modules/jsdoc/jsdoc.js'),
+            confPath = path.resolve(__dirname, './node_modules/angular-jsdoc/conf.json'),
+            templatePath = path.resolve(__dirname, './node_modules/angular-jsdoc/template');
+        var command = [
+            jsdocPath,
+            '-c',
+            confPath,
+            '-t',
+            templatePath,
+            '-d',
+            dest
+        ].concat(paths);
+
+        command = command.join(' ');
+        exec(command, function () {
+            cb();
+        });
+    });
+};
